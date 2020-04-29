c10137f0c2065871ec042a97b9e8f4ab43c318bb
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2020-04-29 12:16:35 +0100

    add extra safety to lib

--- src/lib/getSearchQueryParamsAsObject.ts
+++ src/lib/getSearchQueryParamsAsObject.ts
@@ -1,16 +1,15 @@
 export function getSearchQueryParamsAsObject(str: string): {[key: string]: any} {
   const obj: {[key: string]: any} = {};
-  str
-    .split("?")[1]
-    .split("&")
-    .forEach((n: string) => {
-      const item: string[] = n.split("=");
-      const decodedItem: string = decodeURIComponent(item[1]);
-      try {
-        obj[item[0]] = JSON.parse(decodedItem);
-      } catch {
-        obj[item[0]] = decodedItem;
-      }
-    });
+  const queryString = str.split("?")[1] || "";
+  const items = queryString.split("&");
+  items.forEach((n: string) => {
+    const item: string[] = n.split("=");
+    const decodedItem: string = decodeURIComponent(item[1]);
+    try {
+      obj[item[0]] = JSON.parse(decodedItem);
+    } catch {
+      obj[item[0]] = decodedItem;
+    }
+  });
   return obj;
 }
