5038da1a7aaacebb8f505ef85c61f8ef6ce9e0c3
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2020-04-30 18:15:28 +0100

    WIP: rollup bundling

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
+
+const extensions = [
+  ".js",
+  ".ts"
+];
+
+export default {
+  input: "./src/index.ts",
+  plugins: [
+    resolve({
+      extensions: extensions
+    }),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      extensions: extensions
+    })
+  ],
+  output: [
+    // {
+    //   file: pkg.main,
+    //   format: "umd",
+    //   name: "xapijs"
+    // },
+    {
+      file: pkg.module,
+      format: "es"
+    }
+  ]
+};
