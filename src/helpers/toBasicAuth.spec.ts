b81b5883da79901c449db785c676f708d8fad612
TreyH <vbhayden3@gmail.com>
2021-03-10 13:41:38 -0500

    didn't notice Prettier was in-play

--- src/helpers/toBasicAuth.spec.ts
+++ src/helpers/toBasicAuth.spec.ts
@@ -1,17 +1,21 @@
 import { toBasicAuth } from "./toBasicAuth";
 
 test("converts username and password into Basic Auth header", () => {
-
   const pairs = [
     ["tom", "1234"],
     ["BUG-mb'5#,:,fC83", "4jXGtgwY%\\'xm.k;"],
-    ["Mr}VBHb^)zyc39`<", "?YrAhvP{}\"s94:%%"],
-  ]
+    ["Mr}VBHb^)zyc39`<", '?YrAhvP{}"s94:%%'],
+  ];
 
-  const manualFunction = (pair) => `Basic ${Buffer.from(pair[0] + ":" + pair[1], "binary").toString("base64")}`;
+  const manualFunction = (pair) =>
+    `Basic ${Buffer.from(pair[0] + ":" + pair[1], "binary").toString(
+      "base64"
+    )}`;
   const helperFunction = (pair) => toBasicAuth(pair[0], pair[1]);
 
-  const results = pairs.map(pair => manualFunction(pair) === helperFunction(pair));
+  const results = pairs.map(
+    (pair) => manualFunction(pair) === helperFunction(pair)
+  );
 
   return expect(results).not.toContain(false);
 });
