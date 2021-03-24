7973777a81facefdd4ce9f9abc53ad581268b7eb
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2021-03-24 17:19:39 +0000

    add code coverage collection

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ require("dotenv").config();
 
 module.exports = {
   preset: "ts-jest",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
