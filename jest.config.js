b5d287fb2dde4905f1a976b7c03e91808ca05bd9
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2021-04-26 18:01:54 +0100

    remove coverage requirements

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     },
   },
   projects: [
