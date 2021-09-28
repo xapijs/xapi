73e8fda35ac925075e2a4520fdbb0c71c62d64d1
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2021-09-28 07:57:58 +0100

    simplify test

--- src/XAPI.e2e.spec.ts
+++ src/XAPI.e2e.spec.ts
@@ -12,13 +12,7 @@ credentials.forEach((credential) => {
   describe("xapi constructor", () => {
     test("can perform basic authentication challenges when no authorization process is required", () => {
       const noAuthXapi = new XAPI(endpoint);
-      return noAuthXapi.getAbout().then((result) => {
-        return expect(result.data).toEqual(
-          expect.objectContaining({
-            version: expect.any(Array),
-          })
-        );
-      });
+      expect(noAuthXapi.getAbout()).resolves.toBeDefined();
     });
   });
 });
