5c80b380b1e41336681d6a85e13d87d363300147
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2021-10-19 17:28:14 +0100

    xapi class constructor unit tests

---
+++ src/XAPI.unit.test.ts
@@ -0,0 +1,61 @@
+import { testEndpoint } from "../test/constants";
+import XAPI from "./XAPI";
+import axios from "axios";
+import { toBasicAuth } from "./helpers/toBasicAuth/toBasicAuth";
+import { Versions } from "./constants";
+
+jest.mock("axios");
+
+describe("xapi constructor", () => {
+  beforeEach(() => {
+    (axios as jest.MockedFunction<any>).request.mockResolvedValueOnce({
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  });
+
+  test("can be constructed with an endpoint", () => {
+    const xapi = new XAPI({
+      endpoint: testEndpoint,
+    });
+    xapi.getAbout();
+    expect(axios.request).toHaveBeenCalledWith(
+      expect.objectContaining({
+        headers: expect.objectContaining({
+          Authorization: toBasicAuth("", ""),
+        }),
+      })
+    );
+  });
+
+  test("can be constructed with an endpoint and auth", () => {
+    const xapi = new XAPI({
+      endpoint: testEndpoint,
+      auth: "test",
+    });
+    xapi.getAbout();
+    expect(axios.request).toHaveBeenCalledWith(
+      expect.objectContaining({
+        headers: expect.objectContaining({
+          Authorization: "test",
+        }),
+      })
+    );
+  });
+
+  test("can be constructed with an endpoint and version", () => {
+    const xapi = new XAPI({
+      endpoint: testEndpoint,
+      version: "1.0.0",
+    });
+    xapi.getAbout();
+    expect(axios.request).toHaveBeenCalledWith(
+      expect.objectContaining({
+        headers: expect.objectContaining({
+          "X-Experience-API-Version": "1.0.0" as Versions,
+        }),
+      })
+    );
+  });
+});
